dfeab86944b02333b167220b0f679c0ce5dcb270
Danny Ruchtie <druchtie@gmail.com>
2022-09-29 15:06:46 +0200

    updated url

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export async function getStaticProps() {
   const origin =
     process.env.NODE_ENV !== "production"
       ? "http://localhost:3000"
-      : "https://dutchwho.design/";
+      : "dutch-who-design-uwkv.vercel.app";
 
   console.log(origin)
 
