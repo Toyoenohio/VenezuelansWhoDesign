cdb1d1a7eb38d0a89d1c9312194dbc4e6056765d
Andrés Nicolás Tonello <andresnicolastonello@gmail.com>
2023-01-20 20:41:17 -0300

    Trying to solve decode error on vercel

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,9 @@ const getDesigners = async () => {
   const auth = new google.auth.GoogleAuth({
     credentials: {
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY
+      private_key: process.env.GOOGLE_PRIVATE_KEY.split(String.raw`\n`).join(
+        "\n"
+      )
     },
     scopes: [
       "https://www.googleapis.com/auth/drive",
